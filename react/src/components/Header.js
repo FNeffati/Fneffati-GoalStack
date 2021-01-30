483fc52b62a377c85705cb72bf63958522d8265b
han5010 <error404@live.unc.edu>
2021-01-30 15:26:39 -0500

    added topbar

---
+++ react/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../App.css'
+
+export default () => {
+    
+    return (
+        <div className="topbar">
+            
+            <h1>Goal Stack</h1>
+
+            <div className="loginoptions">
+                <h2 className="loginbuttons">Sign Up</h2>
+                <h2 className="loginbuttons">Log In</h2>
+            </div>
+        </div>
+    )
+    
+}
