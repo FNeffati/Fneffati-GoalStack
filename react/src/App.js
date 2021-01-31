bb68ed0d46b785151e4975c17e69e431a2223901
Dylan Nguyen <dylannguyen1000@gmail.com>
2021-01-30 20:04:46 -0500

    feat(goals): added api requests for goals

--- react/src/App.js
+++ react/src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
-import GetGoal from './GetGoal.js'
 import Header from './components/Header'
-import Tasks from './components/Tasks'
-import Goals from './components/Goals'
-
 import Body from './components/Body'
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
-      <GetGoal />
     </div>
   );
 }
--- react/src/components/Goals.js
+++ react/src/components/Goals.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default () => {
+    const [goals, setGoals] = useState([]);
+
+    useEffect(() => {
+        getGoals();
+    }, []);
+
+    const getGoals = async () => {
+        const goals = await fetch("http://hoyahacks.dylantknguyen.com/api/goals/list/")
+        .then(response => response.json())
+        .then(data => setGoals(data));
+    };
     return (
         <div className="goals">
-            <h1>Goals</h1>
-            <h1>Goals</h1>
-            <h1>Goals</h1>
+            {goals.map((goal) => {
+                const { id, name } = goal;
+                return (
+                    <h1 key={id}>{name}</h1>
+                );
+            })}
         </div>
     )
 }
